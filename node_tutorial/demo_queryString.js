08d4755d404c6a37c24d9f29e9b25b41a108f503
Giacomo-Papaluca <giacomo.papaluca@gmail.com>
2022-04-28 16:44:00 +0200

    chore: node.js tutorial completed

---
+++ node_tutorial/demo_queryString.js
@@ -0,0 +1,13 @@
+var http = require("http");
+var url = require("url");
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    // query returns json Object with the GET query parameters
+    // for example {year: 2017, month: July} in http://localhost:8080/?year=2017&month=July
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.end(txt);
+  })
+  .listen(8080);
